508eb1ffe02a5a0553ba603a5b756261674a4092
MariaPdg <mpodguzova25@gmail.com>
2021-11-07 08:38:17 +0100

    experiments with different splits for cls

--- train_cls.sh
+++ train_cls.sh
@@ -3,14 +3,20 @@
 # chmod +x script.sh
 # ./script.sh &
 #
-echo 'Experiment 1: -ts 20 -vs 20'
-python3 train_classifier.py  -r /home/maria/Study/VisionLabs -ts 20 -vs 20
+echo 'Experiment 1'
+python3 train_classifier.py  -r /home/maria/Study/VisionLabs -ts 80 -vs 20
 sleep 5
-echo 'Experiment 2: -ts 30 -vs 30'
-python3 train_classifier.py  -r /home/maria/Study/VisionLabs -ts 30 -vs 30
+echo 'Experiment 2'
+python3 train_classifier.py  -r /home/maria/Study/VisionLabs -ts 70 -vs 30
 sleep 5
-echo 'Experiment 3: -ts 40 -vs 40'
-python3 train_classifier.py  -r /home/maria/Study/VisionLabs -ts 40 -vs 40
-echo 'Experiment 4: -ts 50 -vs 50'
-python3 train_classifier.py  -r /home/maria/Study/VisionLabs -ts 50 -vs 50
+echo 'Experiment 3'
+python3 train_classifier.py  -r /home/maria/Study/VisionLabs -ts 60 -vs 40
+echo 'Experiment 4'
+python3 train_classifier.py  -r /home/maria/Study/VisionLabs -ts 50 -vs 10
+echo 'Experiment 5'
+python3 train_classifier.py  -r /home/maria/Study/VisionLabs -ts 40 -vs 10
+echo 'Experiment 6'
+python3 train_classifier.py  -r /home/maria/Study/VisionLabs -ts 40 -vs 20
+echo 'Experiment 7'
+python3 train_classifier.py  -r /home/maria/Study/VisionLabs -ts 40 -vs 30
 echo 'Finish training of vae!'
