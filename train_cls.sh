a80a65ae6dad75fa7b4fedb7c582774e6a1c9b8b
MariaPdg <mpodguzova25@gmail.com>
2021-11-09 01:44:46 +0100

    clean up .sh

--- train_cls.sh
+++ train_cls.sh
@@ -4,19 +4,27 @@
 # ./script.sh &
 #
 echo 'Experiment 1'
-python3 train_classifier.py  -r /home/maria/Study/VisionLabs -ts 80 -vs 20
-sleep 5
-echo 'Experiment 2'
-python3 train_classifier.py  -r /home/maria/Study/VisionLabs -ts 70 -vs 30
-sleep 5
+python3 train_classifier.py  -ts 90 -vs 10
 echo 'Experiment 3'
-python3 train_classifier.py  -r /home/maria/Study/VisionLabs -ts 60 -vs 40
+python3 train_classifier.py  -ts 80 -vs 20
 echo 'Experiment 4'
-python3 train_classifier.py  -r /home/maria/Study/VisionLabs -ts 50 -vs 10
+python3 train_classifier.py   -ts 70 -vs 30
 echo 'Experiment 5'
-python3 train_classifier.py  -r /home/maria/Study/VisionLabs -ts 40 -vs 10
+python3 train_classifier.py   -ts 60 -vs 40
 echo 'Experiment 6'
-python3 train_classifier.py  -r /home/maria/Study/VisionLabs -ts 40 -vs 20
+python3 train_classifier.py   -ts 50 -vs 10
 echo 'Experiment 7'
-python3 train_classifier.py  -r /home/maria/Study/VisionLabs -ts 40 -vs 30
-echo 'Finish training of vae!'
+python3 train_classifier.py   -ts 40 -vs 10
+echo 'Experiment 8'
+python3 train_classifier.py   -ts 40 -vs 20
+echo 'Experiment 9'
+python3 train_classifier.py   -ts 40 -vs 30
+echo 'Experiment 10'
+python3 train_classifier.py   -ts 20 -vs 20
+echo 'Experiment 11'
+python3 train_classifier.py   -ts 30 -vs 30
+echo 'Experiment 12'
+python3 train_classifier.py   -ts 40 -vs 40
+echo 'Experiment 13'
+python3 train_classifier.py   -ts 50 -vs 50
+echo 'Finish training of classifier!'
--- train_vae.sh
+++ train_vae.sh
@@ -4,19 +4,19 @@
 # ./script.sh &
 
 echo 'Experiment 1: beta=0.1'
-python3 train_vae.py  -r /home/maria/Study/VisionLabs -be 0.1 -bs 64
+python3 train_vae.py   -be 0.1 -bs 64
 sleep 5
 echo 'Experiment 2: beta=0.01'
-python3 train_vae.py  -r /home/maria/Study/VisionLabs -be 0.01 -bs 64
+python3 train_vae.py   -be 0.01 -bs 64
 sleep 5
 echo 'Experiment 3: beta=0.001'
-python3 train_vae.py  -r /home/maria/Study/VisionLabs -be 0.001 -bs 64
+python3 train_vae.py   -be 0.001 -bs 64
 echo 'Experiment 4: beta=0.1'
-python3 train_vae.py  -r /home/maria/Study/VisionLabs -be 0.1 -bs 128
+python3 train_vae.py   -be 0.1 -bs 128
 sleep 5
 echo 'Experiment 5: beta=0.01'
-python3 train_vae.py  -r /home/maria/Study/VisionLabs -be 0.01 -bs 128
+python3 train_vae.py   -be 0.01 -bs 128
 sleep 5
 echo 'Experiment 6: beta=0.001'
-python3 train_vae.py  -r /home/maria/Study/VisionLabs -be 0.001 -bs 128
+python3 train_vae.py   -be 0.001 -bs 128
 echo 'Finish training of vae!'
