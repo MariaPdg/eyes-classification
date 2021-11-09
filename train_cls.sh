377dc02ef38b9718197fb808a25eb8a2045b13c0
MariaPdg <mpodguzova25@gmail.com>
2021-11-09 22:18:35 +0100

    clean up train_cls.sh

--- train_cls.sh
+++ train_cls.sh
@@ -5,26 +5,16 @@
 #
 echo 'Experiment 1'
 python3 train_classifier.py  -ts 90 -vs 10
-echo 'Experiment 3'
+echo 'Experiment 2'
 python3 train_classifier.py  -ts 80 -vs 20
-echo 'Experiment 4'
+echo 'Experiment 3'
 python3 train_classifier.py   -ts 70 -vs 30
-echo 'Experiment 5'
+echo 'Experiment 4'
 python3 train_classifier.py   -ts 60 -vs 40
-echo 'Experiment 6'
-python3 train_classifier.py   -ts 50 -vs 10
-echo 'Experiment 7'
-python3 train_classifier.py   -ts 40 -vs 10
-echo 'Experiment 8'
-python3 train_classifier.py   -ts 40 -vs 20
-echo 'Experiment 9'
-python3 train_classifier.py   -ts 40 -vs 30
-echo 'Experiment 10'
-python3 train_classifier.py   -ts 20 -vs 20
-echo 'Experiment 11'
+echo 'Experiment 5'
 python3 train_classifier.py   -ts 30 -vs 30
-echo 'Experiment 12'
+echo 'Experiment 6'
 python3 train_classifier.py   -ts 40 -vs 40
-echo 'Experiment 13'
+echo 'Experiment 7'
 python3 train_classifier.py   -ts 50 -vs 50
 echo 'Finish training of classifier!'
